a95bc854a8b9e5d15c5477d57da68e4de049f89d
Barvazur <benamiamit0@gmail.com>
2017-02-01 16:14:51 +0200

    done

--- messages/index.js
+++ messages/index.js
@@ -33,7 +33,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
 .matches('None', (session, args) => {
-    session.send('Hi! This is the None intent handler. You said: \'%s\'.', session.message.text);
+    session.send('\xF0\x9F\x98\x81', session.message.text);
 })
 .matches('Watch', (session, args) => {
     session.send('Hi you motherfucker!!!', session.message.text);
