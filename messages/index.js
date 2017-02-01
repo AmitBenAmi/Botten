6d97195bc37503d5c32c4a0c63957f0246799fc7
Dan Berger <danberger@MacBook-Pro-3.local>
2017-02-01 20:08:40 +0200

    d

--- messages/index.js
+++ messages/index.js
@@ -10,7 +10,10 @@ var botbuilder_azure = require("botbuilder-azure");
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
-var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+var connector = useEmulator ? new builder.ChatConnector({
+    appId: null,
+  appPassword: null
+}) : new botbuilder_azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
     appPassword: process.env['MicrosoftAppPassword'],
     stateEndpoint: process.env['BotStateEndpoint'],
