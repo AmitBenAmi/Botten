3d2936da14a581f2151c711c1d8bcd88c21187ee
shani <you@example.com>
2017-02-02 06:20:41 +0200

    asd

--- messages/index.js
+++ messages/index.js
@@ -156,26 +156,26 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
                 .then(caption => handleSuccessResponse(session, caption))
                 .catch(error => handleErrorResponse(session, error));
             
-            session.send('1', session.message.text);
             var oxfordEmotion = require("node-oxford-emotion")("fbe5dde1aecc4b52aab90285d2dcb2c2");
-            session.send('2', session.message.text);
 
             // Reading in bytes
             var http = require('http');
-            session.send('https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTMcpIopma-Ae8uJxrjUPOKgObGCnkUt9sE8J7LRZk78EbpnsMN4Q', session.message.text);
             http.get('https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTMcpIopma-Ae8uJxrjUPOKgObGCnkUt9sE8J7LRZk78EbpnsMN4Q', (res) => {
                 var imageData = [];
 
                 res.on('data', function (chunk) {
                     imageData.push(chunk);
+                    session.send('2', session.message.text);
                 });
-
+                session.send('1', session.message.text);
                 res.on('end', function () {
+                    session.send('3', session.message.text);
                     var binary = Buffer.concat(imageData);
                     oxfordEmotion.recognize(
                         "image",
                         imageData,
                         function (emotionsRes) {
+                            session.send('4', session.message.text);
                             var emotions = JSON.parse(emotionsRes);
                             var emotionMessage = '';
 
